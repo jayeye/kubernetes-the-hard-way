3beab70864a64a0d820bfe1b304be365a8eecce0
John Ioannidis <ji@symbiont.io>
2020-11-10 01:02:51 -0500

    routes needed explicit dependency on subnet

--- terraform/11-routing.tf
+++ terraform/11-routing.tf
@@ -4,4 +4,5 @@ resource "google_compute_route" "this" {
   network = google_compute_network.this.self_link
   dest_range = cidrsubnet(var.gcp_pod_cidr, 8, count.index)
   next_hop_ip = cidrhost(var.gcp_subnet_cidr, 20 + count.index)
+  depends_on = [google_compute_subnetwork.this]
 }
